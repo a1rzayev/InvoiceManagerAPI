bbea1f313cb9c92002585a683f761178649918e8
a1rzayev <asifrzayev2006@gmail.com>
2025-07-08 21:26:24 +0400

    Create ProductController with CRUD operations

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\ProductController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -35,4 +36,19 @@ Route::prefix('invoices')->group(function () {
     Route::get('/seller/{sellerId}', [InvoiceController::class, 'getBySeller']);
     Route::get('/client/{clientId}', [InvoiceController::class, 'getByClient']);
     Route::patch('/{id}/status', [InvoiceController::class, 'updateStatus']);
+});
+
+
+
+Route::prefix('products')->group(function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::post('/', [ProductController::class, 'store']);
+    Route::get('/{id}', [ProductController::class, 'show']);
+    Route::put('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
+    
+    Route::get('/search/query', [ProductController::class, 'search']);
+    Route::get('/price-range/filter', [ProductController::class, 'getByPriceRange']);
+    Route::get('/expensive/list', [ProductController::class, 'getExpensiveProducts']);
+    Route::get('/cheap/list', [ProductController::class, 'getCheapProducts']);
 });
